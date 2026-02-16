b094f70f301c4caf92ca5529a364a9da0bec4c2b
Madhav Bhardwaj <mb9770@srmist.edu.in>
2026-02-16 05:09:44 +0000

    Palindrome logic written

--- week1/palindrome_Cheker.java
+++ week1/palindrome_Cheker.java
@@ -2,6 +2,26 @@ import java.util.*;
 public class palindrome_Cheker {
     public static void main (String args[]){
         System.out.println("Welcome to the app\n"+"Version 1.0 \n"+"System initialized successfully");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check the palindrome\n");
+        int inp = sc.nextInt();
+        int num = 0;
+        int c = inp;
+        while(inp!=0){
+            
+            int remainder = inp%10;
+            num = num*10 + remainder;
+            inp = inp/10;
+            
+        }
+       
+        if (num == c){
+            System.out.println("The number is a palindrome");
+            
+        }
+        else{
+            System.out.println("The number is not a palindrome");
+        }
 
     }
 
