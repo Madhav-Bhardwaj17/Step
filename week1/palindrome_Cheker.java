505debc95fd995abd09fc7f9a6e2f1dab624868a
Madhav Bhardwaj <mb9770@srmist.edu.in>
2026-02-16 04:23:05 +0000

    Palindrome app version 1 and welcome message

---
+++ week1/palindrome_Cheker.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class palindrome_Cheker {
+    public static void main (String args[]){
+        System.out.println("Welcome to the app\n"+"Version 1.0 \n"+"System initialized successfully");
+
+    }
+
+    
+}
