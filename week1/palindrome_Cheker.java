994156eb764011aea0d9bf51f10a82cb869d14c2
Madhav Bhardwaj <mb9770@srmist.edu.in>
2026-02-23 03:48:23 +0000

    UC1

--- week1/palindrome_Cheker.java
+++ week1/palindrome_Cheker.java
@@ -4,26 +4,8 @@ public class palindrome_Cheker {
         System.out.println("Welcome to the app\n"+"Version 1.0 \n"+"System initialized successfully");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number to check the palindrome\n");
-        int inp = sc.nextInt();
-        int num = 0;
-        int c = inp;
-        while(inp!=0){
-            
-            int remainder = inp%10;
-            num = num*10 + remainder;
-            inp = inp/10;
-            
-        }
-       
-        if (num == c){
-            System.out.println("The number is a palindrome");
-            
-        }
-        else{
-            System.out.println("The number is not a palindrome");
-        }
-
-    }
+        
 
     
+}
 }
