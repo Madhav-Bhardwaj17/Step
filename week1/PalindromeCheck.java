652acacf3ffd30571a1c49987cf04326d71a7828
apple <apple@apples-MacBook-Air-5.local>
2026-03-10 00:34:12 +0530

    implemented the hardcoded string the program

--- week1/PalindromeCheck.java
+++ week1/PalindromeCheck.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class PalindromeCheck {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        
 
-        System.out.println("Enter a string: ");
-        String str = sc.nextLine();
+        
+        String str = "madam";
         String new_str="";
 
         for(int i = str.length()-1 ; i>=0;i--){
@@ -22,6 +22,6 @@ public class PalindromeCheck {
             System.out.println("The given string is not a plaindrome ");
         }
         
-        sc.close();
+      
     }
 }
