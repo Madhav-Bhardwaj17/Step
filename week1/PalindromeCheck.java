da45f2ae0c781ab5a99bf5a7bd928edff3a90365
Madhav Bhardwaj <mb9770@srmist.edu.in>
2026-03-10 03:42:29 +0000

    UC6 implimentation

--- week1/PalindromeCheck.java
+++ week1/PalindromeCheck.java
@@ -1,33 +1,42 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheck {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
+
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        // Push characters into stack
+        // Insert characters into both Stack and Queue
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);     // LIFO
+            queue.add(ch);      // FIFO
         }
 
-        // Pop characters to create reversed string
-        String reversed = "";
+        boolean isPalindrome = true;
+
+        // Compare dequeue from queue and pop from stack
         while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        // Compare original and reversed
-        if (input.equals(reversed)) {
+        // Print result
+        if (isPalindrome) {
             System.out.println("The string is a Palindrome.");
         } else {
             System.out.println("The string is NOT a Palindrome.");
         }
-
         sc.close();
     }
 }
